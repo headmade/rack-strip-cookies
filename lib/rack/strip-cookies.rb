1090e82f18930b006e3ef0c7dde45308aac67084
Lev Rechnik <relevv@gmail.com>
2015-06-26 19:26:51 +0300

    Add option to exclude paths

--- lib/rack/strip-cookies.rb
+++ lib/rack/strip-cookies.rb
@@ -1,19 +1,22 @@
 module Rack
   class StripCookies
-    attr_reader :paths
+    attr_reader :paths, :exclude
 
     def initialize(app, options = {})
-      @app, @paths = app, Array(options[:paths])
+      @app, @paths, @exclude = app, Array(options[:paths]), Array(options[:exclude])
     end
 
     def call(env)
       path     = Rack::Request.new(env).path
       included = paths.any? { |s| path.include?(s)}
 
-      env.delete('HTTP_COOKIE') if included
+      need_delete = paths.any? { |s| path.include?(s)}
+      need_delete &&= !exclude.any? { |s| path.include?(s)}
+
+      env.delete('HTTP_COOKIE') if need_delete
 
       status, headers, body = @app.call(env)
-      headers.delete('Set-Cookie') if included
+      headers.delete('Set-Cookie') if need_delete
 
       [status, headers, body]
     end
